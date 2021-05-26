1249c771c4409ade717b1a8acc26ce5932a5ae2d
Felipe Contreras <felipe.contreras@gmail.com>
2021-05-26 01:56:51 -0500

    Add extra - check
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- lib/parseopt.rb
+++ lib/parseopt.rb
@@ -11,6 +11,7 @@ class ParseOpt
   def parse(args = ARGV)
     args.delete_if do |cur|
       opt = val = nil
+      next false if cur[0] != '-'
       case cur
       when /^-([^-])$/
         opt = @list[$1]
