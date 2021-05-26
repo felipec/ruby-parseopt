5ce01c1e143b45c70a11f4c3ac668471d77f7361
Felipe Contreras <felipe.contreras@gmail.com>
2021-05-26 01:46:43 -0500

    test: check missing arguments
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- test/test_optparse.rb
+++ test/test_optparse.rb
@@ -5,10 +5,22 @@ class ParseOptTest < Test::Unit::TestCase
 
   def test_bool
     bool = false
-    opts = ParseOpt.new
-    opts.on('b') { |v| bool = v }
-    assert_equal(opts.parse(%w[-b]), [])
+    run_opts('b', %w[-b]) { |v| bool = v }
     assert(bool)
+  end
+
+  def test_empty_bool
+    bool = false
+    run_opts('b', []) { |v| bool = v }
+    assert(!bool)
+  end
+
+  private
+
+  def run_opts(opt, args, result = [])
+    opts = ParseOpt.new
+    opts.on(opt) { |v| yield v }
+    assert_equal(result, opts.parse(args))
   end
 
 end
